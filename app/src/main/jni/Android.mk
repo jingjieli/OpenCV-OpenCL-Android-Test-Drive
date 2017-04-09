e32aefe2f5ad3c613e3fd0dd317c24d024cde8ee
Astri <astri@astri.org>
2017-04-09 15:13:04 +0800

    Initial commit: add project files

---
+++ app/src/main/jni/Android.mk
@@ -0,0 +1,30 @@
+LOCAL_PATH := $(call my-dir)
+
+# add OpenCV
+include $(CLEAR_VARS)
+OPENCV_INSTALL_MODULES:=on
+OPENCV_ANDROID_SDK := ./OpenCV
+ifdef OPENCV_ANDROID_SDK
+  ifneq ("","$(wildcard $(OPENCV_ANDROID_SDK)/OpenCV.mk)")
+    include ${OPENCV_ANDROID_SDK}/OpenCV.mk
+  else
+    include ${OPENCV_ANDROID_SDK}/sdk/native/jni/OpenCV.mk
+  endif
+else
+  #include ../../sdk/native/jni/OpenCV.mk
+  include ./OpenCV.mk
+endif
+
+OPENCL_SDK := ./OpenCL
+ifndef OPENCL_SDK
+  $(error Specify OPENCL_SDK to Android OpenCL SDK location)
+endif
+
+# add OpenCL
+LOCAL_C_INCLUDES += $(OPENCL_SDK)/include
+LOCAL_LDLIBS += -L$(OPENCL_SDK)/lib/$(TARGET_ARCH_ABI) -lOpenCL
+
+LOCAL_MODULE    := JNIpart
+LOCAL_SRC_FILES := jni.c CLprocessor.cpp
+LOCAL_LDLIBS    += -llog -lGLESv2 -lEGL
+include $(BUILD_SHARED_LIBRARY)
---
+++ app/src/main/obj/local/armeabi-v7a/objs/JNIpart/jni.o.d
@@ -0,0 +1,14 @@
+/Users/astri/Projects/OpenCVAndroidExamples/tutorial-4-opencl/app/src/main/obj/local/armeabi-v7a/objs/JNIpart/jni.o: \
+ /Users/astri/Projects/OpenCVAndroidExamples/tutorial-4-opencl/app/src/main/jni/jni.c \
+ /Users/astri/Documents/Tools/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/jni.h \
+ /Users/astri/Documents/Tools/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /Users/astri/Documents/Tools/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/astri/Documents/Tools/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/android/api-level.h
+
+/Users/astri/Documents/Tools/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/jni.h:
+
+/Users/astri/Documents/Tools/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/astri/Documents/Tools/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/astri/Documents/Tools/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/android/api-level.h:
